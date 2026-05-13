7566a945a6ca59ec98ba996c5aedf34cc4105d08
likithasatti <likitha2918@gmail.com>
2026-05-13 12:45:56 +0530

    reduce whisper memory usage

--- audiototext.py
+++ audiototext.py
@@ -1,11 +1,17 @@
 from faster_whisper import WhisperModel
 
-model = WhisperModel("base")
+model = None
 
 def transcribe_audio(audio_path):
+    global model
+
+    if model is None:
+        model = WhisperModel("tiny")
+
     segments, info = model.transcribe(audio_path)
 
     text = ""
+
     for segment in segments:
         text += segment.text + " "
 
